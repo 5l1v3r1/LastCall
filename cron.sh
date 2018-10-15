5ad46b4790e95d836252d480882b874bf50e1a0c
Adalen Vladi <adalenvladi@gmail.com>
2018-10-15 12:45:56 +0200

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -1,4 +1,9 @@
-#set location before setup 
+#!/bin/bash
 #cron
 # 0 3 * * * /var/www/html/crm/report4/cron.sh
+ #set location before setup
 /bin/php /var/www/html/crm/report4/report4.php
+cd  /var/www/html/crm/report4/ ;
+/bin/php report4.php ;
+echo "sucess" > /tmp/report4.tmp ;
+
